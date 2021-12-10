274fe36c9e89b5da452d0c6b5b7b74ab4dcf67d9
Lee Suho <suho2718@gmail.com>
2021-12-10 14:06:45 +0900

    init project

---
+++ src/main/java/com/suho/demo/firebaseauth/FirebaseAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.suho.demo.firebaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseAuthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suho/demo/firebaseauth/FirebaseAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suho.demo.firebaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
